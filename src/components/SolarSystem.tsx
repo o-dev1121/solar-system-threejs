5ebff2730b3c76fac7b11782976bab3d76c16382
“o-dev1121” <devstar480@gmail.com>
2025-06-04 15:32:29 -0300

    fix: conserta Sol nao carregando no inicio

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -9,7 +9,7 @@ import Background from './Background';
 import { useGLTF } from '@react-three/drei';
 
 export default memo(function SolarSystem() {
-  const { sun, planets, dwarfPlanets } = useContext(BodyDataContext);
+  const { sun, planets, dwarfPlanets, loading } = useContext(BodyDataContext);
   const { getLayer } = useContext(LayerContext);
   const ambientLightLayer = getLayer('ambient-light');
 
@@ -19,11 +19,13 @@ export default memo(function SolarSystem() {
 
   useGLTF.preload('/models/generic-moon/scene.gltf');
 
+  if (loading) return null;
+
   return (
     <>
       <Background />
 
-      <Sun bodyData={sun as BodyType} />
+      {<Sun bodyData={sun as BodyType} />}
 
       {allPlanets?.map((planet) => (
         <Planet key={planet.id} bodyData={planet} />
