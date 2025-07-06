d4c56c491db260a1dbea9899c909c11478d7a2c3
“o-dev1121” <devstar480@gmail.com>
2025-07-06 16:07:33 -0300

    feat: adiciona responsividade e novas fotos a tela de loading

--- src/components/views/LoadingScreen.tsx
+++ src/components/views/LoadingScreen.tsx
@@ -3,47 +3,69 @@ import Tagline from '../../assets/logo/tagline.svg?react';
 
 const bgImgs = [
   {
-    src: '/img/cover1.png',
+    srcSet: [
+      '/img/cover1_1920w.webp',
+      '/img/cover1_1080w.webp',
+      '/img/cover1_600w.webp',
+    ],
     alt: 'Saturno e seus anéis',
   },
   {
-    src: '/img/cover2.png',
+    srcSet: [
+      '/img/cover2_1920w.webp',
+      '/img/cover2_1080w.webp',
+      '/img/cover2_600w.webp',
+    ],
+    alt: 'Planeta Terra com o Sol ao fundo',
+  },
+  {
+    srcSet: [
+      '/img/cover3_1920w.webp',
+      '/img/cover3_1080w.webp',
+      '/img/cover3_600w.webp',
+    ],
+    alt: 'Planeta Terra com o cometa Halley ao fundo',
+  },
+  {
+    srcSet: [
+      '/img/cover4_1920w.webp',
+      '/img/cover4_1080w.webp',
+      '/img/cover4_600w.webp',
+    ],
     alt: 'Planeta Terra',
-  },
-  {
-    src: '/img/cover3.png',
-    alt: 'Terra com o Sol ao fundo',
-  },
-  {
-    src: '/img/cover4.png',
-    alt: 'Lua com a Terra ao fundo',
   },
 ];
 
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 h-full w-full overflow-hidden bg-black">
-      {bgImgs.map(({ src, alt }, index) => (
-        <img
+      {bgImgs.map(({ srcSet, alt }, index) => (
+        <picture
           key={index}
-          className="animate-fadeCycle absolute h-full w-full object-cover opacity-0"
+          className="animate-fadeCycle absolute h-full w-full opacity-0"
           style={{
             animationDuration: `${bgImgs.length * 3}s`,
             animationDelay: `${index * 3}s`,
           }}
-          src={src}
-          alt={alt}
-        />
+        >
+          <source media="(width > 1080px)" srcSet={srcSet[0]} />
+          <source media="(width < 600px)" srcSet={srcSet[2]} />
+          <img
+            className="h-full w-full object-cover"
+            src={srcSet[1]}
+            alt={alt}
+          />
+        </picture>
       ))}
 
       <div className="absolute inset-0 z-10">
-        <div className="flex h-full flex-col place-content-center items-center gap-6">
+        <div className="flex h-full flex-col place-content-center items-center gap-6 px-8 min-[25rem]:px-20">
           <Logo
-            className="animate-logoIn w-[30rem] opacity-0"
+            className="animate-logoIn max-w-[30rem] min-w-30 opacity-0"
             style={{ animationDelay: '1s' }}
           />
           <Tagline
-            className="animate-logoIn w-[18rem] opacity-0"
+            className="animate-logoIn max-w-[18rem] min-w-30 opacity-0"
             style={{ animationDelay: '1.3s' }}
           />
         </div>
